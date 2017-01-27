cd27ce1bc8541dc67823e96e04eb84069767eaaf
Matt Fenwick <mfenwick100@gmail.com>
2017-01-27 09:40:07 -0600

    Initial Commit

---
+++ immutable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  immutable
+//
+//  Created by Matt Fenwick on 1/27/17.
+//  Copyright © 2017 mf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
